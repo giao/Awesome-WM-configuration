50dacc29cd15b1c034adb5472c9a0a6a70d32824
Egor Churaev <egor.churaev@intel.com>
2017-09-04 10:04:56 +0300

    Enabled suspend when press on sleep button

--- rc.lua
+++ rc.lua
@@ -112,7 +112,7 @@ myawesomemenu = {
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
                                     { "Debian", debian.menu.Debian_menu.Debian, beautiful.debian_icon },
                                     { "Open terminal", terminal, beautiful.terminal_icon },
-                                    { "Sleep", terminal, beautiful.sleep_icon },
+                                    { "Sleep", function() awful.util.spawn_with_shell("systemctl suspend") end, beautiful.sleep_icon },
                                     { "Restart", function() awful.util.spawn_with_shell("restart") end, beautiful.restart_icon },
                                     { "Shut Down", function() awful.util.spawn_with_shell("poweroff") end, beautiful.shutdown_icon }
                                   }
