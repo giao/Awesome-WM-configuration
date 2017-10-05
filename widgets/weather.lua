2442cd5f3ce3ece00122e98d56edbe8c48344891
Egor Churaev <egor.churaev@intel.com>
2017-10-05 14:40:38 +0300

    Configured weather widget

--- widgets/weather.lua
+++ widgets/weather.lua
@@ -2,7 +2,6 @@ local wibox = require("wibox")
 local vicious = require("vicious")
 
 local weather = wibox.widget.textbox()
-vicious.register(weather, vicious.widgets.weather, "Weather: ${city} | Temp: ${tempc}⁰C | Humid: ${humid}%", 1200, "KPVD")
----vicious.register(weather, vicious.widgets.weather, "Weather: ${city}.  Sky: ${sky}. Temp: ${tempc}c Humid: ${humid}%. Wind: ${windkmh} KM/h", 1200, "LFBO")
+vicious.register(weather, vicious.widgets.weather, "Weather: ${city}, Temp: ${tempc}⁰C", 1200, "UWGG")
 
 return weather
